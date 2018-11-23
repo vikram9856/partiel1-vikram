e9a7194e4b6e4fa1086a11589a5c0261d98682c6
[vikram] <[vikram.rungloll98@gmail.com]>
2018-11-23 17:21:52 +0100

    4 commit push

--- partiel1-vikram/src/main/java/fr/formation/partiel1/Rib.java
+++ partiel1-vikram/src/main/java/fr/formation/partiel1/Rib.java
@@ -5,35 +5,22 @@ public class Rib {
 	String codeGuichet;
 	String numeroDeCompte;
 	String cleRib;
-
 	
-
-
 	public Rib(String codeBanque, String codeGuichet, String numeroDeCompte, String cleRib) {
 		// TODO Auto-generated constructor stub
 	}
-
-
-
 
 	public String getCodeBanque() {
 		return codeBanque;
 	}
 
-
-
-
 	public String getCodeGuichet() {
 		return codeGuichet;
 	}
-
-
-
-
+	
 	public String getNumeroDeCompte() {
 		return numeroDeCompte;
 	}
-
 
 
 
@@ -41,36 +28,20 @@ public class Rib {
 		return cleRib;
 	}
 
-
-
-
 	public void setCodeBanque(String codeBanque) {
 		this.codeBanque = codeBanque;
 	}
-
-
-
 
 	public void setCodeGuichet(String codeGuichet) {
 		this.codeGuichet = codeGuichet;
 	}
 
-
-
-
 	public void setNumeroDeCompte(String numeroDeCompte) {
 		this.numeroDeCompte = numeroDeCompte;
 	}
 
-
-
-
 	public void setCleRib(String cleRib) {
 		this.cleRib = cleRib;
 	}
-
-
-	
-	
 
 }
